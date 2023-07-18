3e57be1495cb1fa07a41e1fb48d6448c39b8c2d9
LightQv <QVivian23@gmail.com>
2023-07-18 18:26:17 +0200

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         {/* Public routes */}
         <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Profile />} />
         <Route path="/profile" element={<Profile />} />
-        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
       <ToastContainer limit={1} />
     </>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,13 +5,14 @@ export default function Profile() {
   const { user, logout } = useUserContext();
 
   return (
-    <main className="bg-dust-0 h-screen w-screen flex flex-col justify-center font-inter">
+    <main className="bg-dust-0 h-screen w-screen flex flex-col justify-between font-inter">
       {user?.id ? (
         <>
           <p>Hello</p>
           <button type="button" onClick={logout}>
             Logout
           </button>
+          <p>cc</p>
         </>
       ) : (
         <Authentification />
