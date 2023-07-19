f8ecceb2c505bd4bbcacec23a6342ed76da793c1
LightQv <QVivian23@gmail.com>
2023-07-19 18:25:24 +0200

    delete post done

--- frontend/src/components/profile/PostInsight.jsx
+++ frontend/src/components/profile/PostInsight.jsx
@@ -1,14 +1,31 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
 
-export default function PostInsight({ data, index }) {
+export default function PostInsight({ data, index, loading, setLoading }) {
   return (
     <li className="h-full w-full">
-      <div className="flex h-[50dvw] w-full items-center justify-center lg:h-60">
+      <div
+        className="flex h-[50dvw] w-full items-center justify-center lg:h-60"
+        style={{ display: loading ? "flex" : "none" }}
+      >
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="25%"
+          visible
+        />
+      </div>
+      <div
+        className="flex h-[50dvw] w-full items-center justify-center lg:h-60"
+        style={{ display: loading ? "none" : "block" }}
+      >
         <Link to={`/post-details/${data.post_id}`} className="h-full w-full">
           <img
             src={data.gif_url}
             alt={`Post ${index + 1}`}
+            onLoad={() => setLoading(false)}
             className="h-full w-full rounded-md object-cover"
           />
         </Link>
@@ -20,4 +37,6 @@ export default function PostInsight({ data, index }) {
 PostInsight.propTypes = {
   data: PropTypes.shape().isRequired,
   index: PropTypes.number.isRequired,
+  loading: PropTypes.bool.isRequired,
+  setLoading: PropTypes.func.isRequired,
 };
