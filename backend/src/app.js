4753c5340490c38f608e964a92deb386dab89a99
LightQv <QVivian23@gmail.com>
2023-11-28 14:05:15 +0100

    added cors on vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     optionsSuccessStatus: 200,
     credentials: true,
   })
