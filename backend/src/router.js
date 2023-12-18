7f9162bd69ef086525b85e0423e4daba21c0f516
LightQv <QVivian23@gmail.com>
2023-12-18 13:03:38 +0100

    fixed prod build

--- backend/src/router.js
+++ backend/src/router.js
@@ -29,16 +29,21 @@ const followControllers = require("./controllers/followControllers");
 
 // --- Public Routes (without Auth) --- //
 // Login & Register
-router.post("/api/login", getUserByEmailMiddleware, verifyPassword);
-router.post("/api/register", validateUser, hashPassword, userControllers.add);
+router.post("/api/v1/login", getUserByEmailMiddleware, verifyPassword);
 router.post(
-  "/api/forgotten-password",
+  "/api/v1/register",
+  validateUser,
+  hashPassword,
+  userControllers.add
+);
+router.post(
+  "/api/v1/forgotten-password",
   getUserByEmailMiddleware,
   generatePasswordToken,
   sendForgottenPassword
 );
 router.post(
-  "/api/reset-password",
+  "/api/v1/reset-password",
   verifyPasswordToken,
   validateEditUserPw,
   hashPassword,
@@ -46,70 +51,80 @@ router.post(
 );
 
 // Posts with Likes & Feelings
-router.get("/api/posts", postControllers.browse);
-router.get("/api/posts/:id", postControllers.readWithUser);
-router.get("/api/likes-post/:id", likeControllers.browseByPost);
-router.get("/api/feelings-post/:id", feelingControllers.browseByPost);
+router.get("/api/v1/posts", postControllers.browse);
+router.get("/api/v1/posts/:id", postControllers.readWithUser);
+router.get("/api/v1/likes-post/:id", likeControllers.browseByPost);
+router.get("/api/v1/feelings-post/:id", feelingControllers.browseByPost);
 
 // -> For User's Search-List
-router.get("/api/users", userControllers.browse);
+router.get("/api/v1/users", userControllers.browse);
 
 // Users's profiles
-router.get("/api/users/:username", userControllers.readByUsername);
+router.get("/api/v1/users/:username", userControllers.readByUsername);
 
 // Stats for each User
-router.get("/api/followers-stats/:id", followControllers.countFollowerByUser);
-router.get("/api/follows-stats/:id", followControllers.countFollowsByUser);
-router.get("/api/likes-stats/:id", likeControllers.countByUser);
-router.get("/api/feelings-stats/:id", feelingControllers.countByUser);
-router.get("/api/followers-list/:id", userControllers.browseFollowersByUser);
-router.get("/api/follows-list/:id", userControllers.browseFollowsByUser);
+router.get(
+  "/api/v1/followers-stats/:id",
+  followControllers.countFollowerByUser
+);
+router.get("/api/v1/follows-stats/:id", followControllers.countFollowsByUser);
+router.get("/api/v1/likes-stats/:id", likeControllers.countByUser);
+router.get("/api/v1/feelings-stats/:id", feelingControllers.countByUser);
+router.get("/api/v1/followers-list/:id", userControllers.browseFollowersByUser);
+router.get("/api/v1/follows-list/:id", userControllers.browseFollowsByUser);
 
 // --- Private Routes (Auth requiered) --- //
 router.use(verifyToken);
 // Logout
-router.get("/api/logout", logout);
+router.get("/api/v1/logout", logout);
 
 // Edit User's Profile
-router.put("/api/users-avatar/:id", userControllers.editAvatar);
-router.put("/api/users-ml/:id", validateEditUserMail, userControllers.editMail);
+router.put("/api/v1/users-avatar/:id", userControllers.editAvatar);
 router.put(
-  "/api/users-pw/:id",
+  "/api/v1/users-ml/:id",
+  validateEditUserMail,
+  userControllers.editMail
+);
+router.put(
+  "/api/v1/users-pw/:id",
   validateEditUserPw,
   hashPassword,
   userControllers.editPw
 );
 router.put(
-  "/api/users-profile/:id",
+  "/api/v1/users-profile/:id",
   validateEditProfile,
   userControllers.editUsername
 );
-router.delete("/api/users/:id", userControllers.destroy);
+router.delete("/api/v1/users/:id", userControllers.destroy);
 
 // Auth's User's Followed Posts
-router.get("/api/posts-followed/:id", postControllers.browseByFollow);
-router.get("/api/users-unfollowed/:id", userControllers.browseUnfollowedUser);
+router.get("/api/v1/posts-followed/:id", postControllers.browseByFollow);
+router.get(
+  "/api/v1/users-unfollowed/:id",
+  userControllers.browseUnfollowedUser
+);
 
 // Handle Liked Posts
-router.get("/api/posts-liked/:id", postControllers.browseByUserLikes);
+router.get("/api/v1/posts-liked/:id", postControllers.browseByUserLikes);
 
 // Posts's CRUD
-router.post("/api/posts", postControllers.add);
-router.put("/api/posts/:id", postControllers.edit);
-router.delete("/api/posts/:id", postControllers.destroy);
+router.post("/api/v1/posts", postControllers.add);
+router.put("/api/v1/posts/:id", postControllers.edit);
+router.delete("/api/v1/posts/:id", postControllers.destroy);
 
 // Likes's CD
-router.post("/api/likes", likeControllers.add);
-router.delete("/api/likes/:id", likeControllers.destroy);
+router.post("/api/v1/likes", likeControllers.add);
+router.delete("/api/v1/likes/:id", likeControllers.destroy);
 
 // Feelings's CD
-router.post("/api/feelings", feelingControllers.add);
-router.delete("/api/feelings/:id", feelingControllers.destroy);
+router.post("/api/v1/feelings", feelingControllers.add);
+router.delete("/api/v1/feelings/:id", feelingControllers.destroy);
 
 // Follow's CD
-router.post("/api/follows", followControllers.add);
+router.post("/api/v1/follows", followControllers.add);
 router.delete(
-  "/api/follows/:followerId&:followingId",
+  "/api/v1/follows/:followerId&:followingId",
   followControllers.destroy
 );
 
